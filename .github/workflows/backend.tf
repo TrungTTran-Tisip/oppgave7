9bbf551a7bb50c7bc22fb97e378947847d137510
trungttt <thetrung1268@gmail.com>
2025-10-20 17:47:34 +0200

    changes

--- .github/workflows/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "trung-demo-rg"  # Update with your RG name if different
-    storage_account_name = "trungdemostorage"  # Update with your storage account name
-    container_name       = "tfstate"  # Update if different
-    key                  = "terraform.tfstate"
-  }
-}
