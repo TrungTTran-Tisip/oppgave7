2e072569bc60a307f8083253600454f7ad4104dc
trungttt <thetrung1268@gmail.com>
2025-10-20 14:56:18 +0200

    Update main.tf

--- terraform-projects/rg/main.tf
+++ terraform-projects/rg/main.tf
@@ -15,9 +15,6 @@ provider "azurerm" {
 resource "azurerm_resource_group" "fd-rg" {
   name     = var.rg_name
   location = var.location
-  tags = {
-    Environment = "${github.ref_name}"
-  }
 }
 variable "rg_name" {
   description = "The name of the resource group"
