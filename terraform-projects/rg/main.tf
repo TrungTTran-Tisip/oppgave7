5ce93a43d94a7b3bae41620edd6a732f398a4b3e
trungttt <thetrung1268@gmail.com>
2025-10-20 17:46:50 +0200

    Add Terraform files for Oppgave 7.2

--- terraform-projects/rg/main.tf
+++ terraform-projects/rg/main.tf
@@ -10,12 +10,3 @@ resource "azurerm_resource_group" "example" {
     Owner       = "trung"
   }
 }
-variable "rg_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "The location of the resource group"
-  type        = string
-}
