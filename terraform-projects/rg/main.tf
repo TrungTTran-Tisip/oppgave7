530507d5475190d1041659c90b558c096f689926
trungttt <thetrung1268@gmail.com>
2025-10-20 14:55:05 +0200

    Update main.tf

--- terraform-projects/rg/main.tf
+++ terraform-projects/rg/main.tf
@@ -19,3 +19,12 @@ resource "azurerm_resource_group" "fd-rg" {
     Environment = "${github.ref_name}"
   }
 }
+variable "rg_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resource group"
+  type        = string
+}
