331608ca14d720ec50b26d9ca6be123e9605ef07
trungttt <thetrung1268@gmail.com>
2025-10-20 15:40:08 +0200

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,24 +1,21 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 4.40.0"
-    }
-  }
-}
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = var.rgname
+  name     = var.rg_name
   location = var.location
+  tags = {
+    Environment = "dev"
+    Owner       = "trung"
+  }
 }
-
-variable "rgname" {
-  type = string
+variable "rg_name" {
+  description = "The name of the resource group"
+  type        = string
 }
 
 variable "location" {
-  type = string
+  description = "The location of the resource group"
+  type        = string
 }
